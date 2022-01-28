3f3d0136b4ead8aaea3819e98f85d60e8cbf2249
rohit-rambade <rohitrambade18.com>
2022-01-28 17:30:10 +0530

    init

---
+++ src/components/header/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './header.css'
+import Mobile from './mobile';
+import Web from './web';
+function Header() {
+  const [isOpen,setIsOpen]=useState(false);
+  return <div className='header'>
+      <div className='logo'>Rohit Rambade logo</div>   
+        <div className='menu'>
+          <div className='web-menu'>
+            <Web/>
+          </div>
+          <div className='mobile-menu'> 
+          <div onClick={()=>setIsOpen(!isOpen)}>
+            <i class="fi fi-rr-align-justify menu-icon"></i>
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen}/>}
+          </div>
+        </div>   
+  </div>;
+}
+
+export default Header;
