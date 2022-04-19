6cfd15df44b9da489384fba0db4a73e740c45e64
sahil gaonkar <sahilgaonkar94058@gmail.com>
2022-04-19 18:27:18 +0530

    Updates

---
+++ precache-manifest.7ad3a2332121bfb0a282d1b74d0d3eb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "49feb276e4f95afec250",
+    "url": "/ReactPortfolio/static/css/main.1e98de0a.chunk.css"
+  },
+  {
+    "revision": "49feb276e4f95afec250",
+    "url": "/ReactPortfolio/static/js/main.49feb276.chunk.js"
+  },
+  {
+    "revision": "17fb29ed40cd8c1083bf",
+    "url": "/ReactPortfolio/static/js/1.17fb29ed.chunk.js"
+  },
+  {
+    "revision": "bb5f21b3670b3090b3c7",
+    "url": "/ReactPortfolio/static/js/2.bb5f21b3.chunk.js"
+  },
+  {
+    "revision": "ecbaf57d6decb307487b",
+    "url": "/ReactPortfolio/static/js/runtime~main.ecbaf57d.js"
+  },
+  {
+    "revision": "c492c4858277d7cdf161f4fc8f1e8088",
+    "url": "/ReactPortfolio/static/media/instagram.c492c485.png"
+  },
+  {
+    "revision": "f23714d05020e83cc917c043bf1721f7",
+    "url": "/ReactPortfolio/static/media/github.f23714d0.png"
+  },
+  {
+    "revision": "a378538e7b1cefa51b56740cc384562e",
+    "url": "/ReactPortfolio/static/media/coding.a378538e.png"
+  },
+  {
+    "revision": "99ac291dca07fdadd0b2c7a9cdbec98e",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
